6ed9948d02458fdd70c93771b4d15396a24461c3
patrick793 <patrick7932003@yahoo.com>
2018-06-11 02:48:15 +0800

    Adding delete all flows method in sample.py

--- sample.py
+++ sample.py
@@ -428,6 +428,29 @@ class SimpleSwitch13(app_manager.RyuApp):
         #                      stat.rx_packets, stat.rx_bytes, stat.rx_errors,
         #                      stat.tx_packets, stat.tx_bytes, stat.tx_errors)
 
+ def remove_flows(self, datapath, table_id):
+        """Removing all flow entries."""
+        parser = datapath.ofproto_parser
+        ofproto = datapath.ofproto
+        empty_match = parser.OFPMatch()
+        instructions = []
+        flow_mod = self.remove_table_flows(datapath, table_id,
+                                        empty_match, instructions)
+        print "deleting all flow entries in table ", table_id
+        datapath.send_msg(flow_mod)
+
+def remove_table_flows(self, datapath, table_id, match, instructions):
+        """Create OFP flow mod message to remove flows from table."""
+        ofproto = datapath.ofproto
+        flow_mod = datapath.ofproto_parser.OFPFlowMod(datapath, 0, 0, table_id,
+                                                      ofproto.OFPFC_DELETE, 0, 0,
+                                                      1,
+                                                      ofproto.OFPCML_NO_BUFFER,
+                                                      ofproto.OFPP_ANY,
+                                                      OFPG_ANY, 0,
+                                                      match, instructions)
+        return flow_mod
+
     @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
     def switch_features_handler(self, ev):
         datapath = ev.msg.datapath
@@ -435,6 +458,8 @@ class SimpleSwitch13(app_manager.RyuApp):
         parser = datapath.ofproto_parser
 
         dpid = datapath.id
+
+        self.remove_flows(datapath, n) for n in [0, 1]]
 
         # install table-miss flow entry
         #
